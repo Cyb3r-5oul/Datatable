848ee7b546f1035d21336b50d1af5a0e17559777
Cyb3r-5oul <78719843+Cyb3r-5oul@users.noreply.github.com>
2021-03-04 09:23:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function load_data() {
-	var data = fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json").then(res => res.json()).then(data)
+	link = "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json"
+	fill(link)
+	
+}
 
+function fill(link) {
+	var data = fetch(link).then(res => res.json()).then(data)
 	data.then(value => {
 		for(i=0;i<9;i++){
 			element = document.getElementById('row'+i)
-			
-			//element = document.createElement('p')
-
+		
 			console.log(element.children[0].innerHTML)
 			element.children[0].innerHTML = value[i].date
 			element.children[1].innerHTML = value[i].firstName
@@ -14,61 +17,24 @@ function load_data() {
 			element.children[3].innerHTML = value[i].lastName
 			element.children[4].innerHTML = value[i].location
 			element.children[5].innerHTML = value[i].salary
-			
-			
-
-			
 		}
-	})
+		})	
 }
+
+
 
 function content(val) {
 	if(val == 1) {
-		var data = fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json").then(res => res.json()).then(data)
-		data.then(value => {
-		for(i=0;i<9;i++){
-			element = document.getElementById('row'+i)
-
-			console.log(element.children[0].innerHTML)
-			element.children[0].innerHTML = value[i].date
-			element.children[1].innerHTML = value[i].firstName
-			element.children[2].innerHTML = value[i].id
-			element.children[3].innerHTML = value[i].lastName
-			element.children[4].innerHTML = value[i].location
-			element.children[5].innerHTML = value[i].salary
-		}
-
-		})
+		link = "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json"
+		fill(link)
 	}
 	else if(val == 2){
-		var data = fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/medium.json").then(res => res.json()).then(data)
-		data.then(value => {
-		for(i=0;i<9;i++){
-			element = document.getElementById('row'+i)
-			console.log(element.children[0].innerHTML)
-			element.children[0].innerHTML = value[i].date
-			element.children[1].innerHTML = value[i].firstName
-			element.children[2].innerHTML = value[i].id
-			element.children[3].innerHTML = value[i].lastName
-			element.children[4].innerHTML = value[i].location
-			element.children[5].innerHTML = value[i].salary	
-		}
-	})
+		link = "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/medium.json"
+		fill(link)
 	}
 	
 	else if(val == 3) {
-		var data = fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/large.json").then(res => res.json()).then(data)
-		data.then(value => {
-		for(i=0;i<9;i++){
-			element = document.getElementById('row'+i)
-			console.log(element.children[0].innerHTML)
-			element.children[0].innerHTML = value[i].date
-			element.children[1].innerHTML = value[i].firstName
-			element.children[2].innerHTML = value[i].id
-			element.children[3].innerHTML = value[i].lastName
-			element.children[4].innerHTML = value[i].location
-			element.children[5].innerHTML = value[i].salary	
-		}
-	})
+		link = "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/large.json"
+		fill(link)
 	}
 }
